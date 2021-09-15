6660c4153ecaef75480857ce97f46e6f16edf1f6
MatBry1337 <74667470+MatBry1337@users.noreply.github.com>
2021-09-15 16:33:47 +0200

    Update main.py
    Updated come

--- main.py
+++ main.py
@@ -10,15 +10,9 @@ r = requests.get(link)
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-tag1 = soup.find_all('a', href=re.compile('topics'))
-tag2 = soup.find_all('a', href=re.compile('entity'))
+tag1 = soup.find_all('a', href=re.compile('topics' | 'entity'))
 
 for i in tag1:
-    if len(i.text) > 1:
-        if i.text.lower().startswith('S'):
-            links.append(i.text)
-
-for i in tag2:
     if len(i.text) > 1:
         if i.text.lower().startswith('S'):
             links.append(i.text)
